f68e3d7453a04faaff5a2f0e45d209da18373d8e
unknown <akash2002155@gmail.com>
2024-02-16 18:56:44 +0530

    MongoDB connected

--- api/index.js
+++ api/index.js
@@ -1,11 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
 
-
 import dotenv from "dotenv";
 dotenv.config();
+
+
+// DataBase connection
+mongoose.connect(process.env.DB_CONNECT,{
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+}).then(() => console.log("DB connected")).catch((err) => console.log(err));
 
 const app = express();
 const PORT = 8800;
 
-app.listen(PORT, () => {console.log(`server is listening on port ${PORT}`)});
+app.listen(PORT, () => {
+  console.log(`server is listening on port ${PORT}`);
+});
